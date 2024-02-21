fc065bb0c2884d751be60fee7260771d836ffc56
Thomas Verderesi <tverderesi@gmail.com>
2024-02-21 00:13:41 -0300

    adds db and db tests

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,31 @@
 import express from "express";
 import router from "./routes";
-
+import mongoMemServer from "./db";
+import mongoose from "mongoose";
 const app = express();
 
 app.use(router);
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
-});
+(async () => {
+  // Spinning up the database
+  const db = await mongoMemServer();
+  console.log("Database is up");
+
+  //Connecting the database to mongoose
+  const mongooseDb = await mongoose.connect(db.getUri());
+  console.log("Database is connected");
+
+  app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+  });
+
+  // Handling the database shutdown
+  process.on("SIGINT", async () => {
+    console.log("Gracefully shutting down...");
+
+    await mongooseDb.disconnect();
+    await db.stop();
+
+    process.exit(0);
+  });
+})();
